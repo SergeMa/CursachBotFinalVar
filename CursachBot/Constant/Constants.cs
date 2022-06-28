006a96a14144e355f1cab57c2219dd9992d4b10f
SergeMa <xxxsergeyxxx77@gmail.com>
2022-06-28 23:10:07 +0300

    Add project files.

---
+++ CursachBot/Constant/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursachBot.Constants
+{
+    class Constants
+    {
+        //public static string Address = "https://localhost:5001";
+        public static string Address = "https://dinalcursachapi.herokuapp.com";
+
+    }
+}
